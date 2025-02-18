c4a52f8c3002e34813f73d65d809ee6af4c4b7f0
cmislivec7 <connormislivec@unomaha.edu>
2025-02-17 18:24:53 -0600

    Update assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+# Our import statements for this problem
+import pandas as pd
+import numpy as np
+import patsy as pt
+
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+test_data = pd.read_csv("assignment2test.csv")
+train_data = pd.read_csv("assignment2train.csv")
+
+y = train_data['meal']
+x = train_data.drop(['meal','id','DateTime'], axis = 1)
+
+model = DecisionTreeClassifier(max_depth=100, min_samples_leaf=10)
+
+modelFit = model.fit(x,y)
+
+
+xt = test_data.drop(['meal', 'id', 'DateTime'], axis=1)
+
+pred = modelFit.predict(xt)
